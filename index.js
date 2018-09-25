8146fb2180607f7216cf2cead24c70498d3feb7c
khanguslee <angus.lee809@gmail.com>
2018-09-25 15:25:37 +1000

    Remove unnecessary prints

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ let stick = new Ant.GarminStick3();
 let bicyclePowerSensor = new Ant.BicyclePowerSensor(stick);
 
 // Set up python shell
-console.log("Setting up python shell");
 var python_script_path = path.join(__dirname, "MHP_Modelling", "Python", "rider_sim_ford_ant_plus.py");
 var spawn = require('child_process').spawn;
 
@@ -17,10 +16,9 @@ var prev_estimated_speed = null, prev_estimated_distance = null;
 var data_count = 0;
 var average_power_total = 0;
 
+// Runs a python script which estimates the final speed and distance given power data
 function calculate_estimated_speed_and_distance(){
     var average_power = average_power_total/data_count;
-    console.log("AVERAGE: " + average_power);
-
     data_count = 0;
     average_power_total = 0;
     var output_arguments = [python_script_path, average_power];
@@ -29,15 +27,13 @@ function calculate_estimated_speed_and_distance(){
         output_arguments.push(prev_estimated_speed);
         output_arguments.push(prev_estimated_distance);
     }
-    console.log(output_arguments);
     var python_process = spawn('python', output_arguments);
 
     python_process.stdout.on('data', (data) => {
         var script_output = JSON.parse(data);
         prev_estimated_speed = script_output["estimated_speed"];
         prev_estimated_distance = script_output["estimated_distance"];
-
-        console.log(script_output);
+        console.log("Power: " + average_power + " W\tEstimated Speed: " + prev_estimated_speed*3.6 + " kmh\t\tEstimated Distance: " +  prev_estimated_distance + " m");
     });
 
     python_process.stderr.on('data', (data) => {
@@ -49,10 +45,7 @@ function calculate_estimated_speed_and_distance(){
 bicyclePowerSensor.on('powerData', data => {
     data_count += 1;
     var bicycle_power = data.Power;
-    //console.log(bicycle_power);
     average_power_total += bicycle_power;
-
-    var python_process;
 
     if (is_initial) {
         is_initial = false;
@@ -66,6 +59,7 @@ stick.on('startup', function () {
 	bicyclePowerSensor.attach(0, 0);
 });
 
+console.log('Opening ant+ USB...');
 if (!stick.open()) {
 	console.log('Stick not found!');
 }
